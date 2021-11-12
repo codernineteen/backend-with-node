0a231fd93003782aa34c21ba86051020c0107f15
Yechan  Jeon <65900882+codernineteen@users.noreply.github.com>
2021-11-12 09:20:45 +0900

    initial commit

--- 10-e-commerce-api/final/utils/checkPermissions.js
+++
@@ -1,7 +0,0 @@
-const checkPermissions = (user, item) => {
-  if (user.role === 'admin') return true;
-  if (user.userId === item.user.toString()) return true;
-  return false;
-};
-
-module.exports = checkPermissions;
