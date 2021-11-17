bfa3e12d3ca81d7aec22f8d8deb2bea79e609eca
Yechan  Jeon <65900882+codernineteen@users.noreply.github.com>
2021-11-17 22:06:10 +0900

    asynchronous pattern

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,19 +1,43 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs').promises;
 
 
-const getText = (path) => {
-    return new Promise((resolve,reject) => {
-        readFile(path, 'utf-8', (err, data) => {
-            if(err) {
-                reject(err)
-            }
-            else {
-                resolve(data)
-            }
-        })
-    })
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf-8')
+        const second = await readFile('./content/second.txt', 'utf-8')
+        await writeFile(
+            './content/result-mind-grenade.txt', 
+            'This is with promise',
+            { flag: 'a' }
+            )
+        console.log(first, second)
+    }
+    catch(err) {
+        console.log(err)
+    }
 }
 
-getText('./content/first.txt')
-.then(result => console.log(result)
-.catch(err))
+start()
+
+//const util = require('util');
+
+//const readFilePromise = util.promisify(readFile)
+//const writeFilePromise = util.promisify(writeFile)
+
+
+// const getText = (path) => {
+//     return new Promise((resolve,reject) => {
+//         readFile(path, 'utf-8', (err, data) => {
+//             if(err) {
+//                 reject(err)
+//             }
+//             else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+// .then(result => console.log(result)
+// .catch(err))
