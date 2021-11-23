98595aaba2ed8c869de51f7416aa7cdbae7824f7
Yechan  Jeon <65900882+codernineteen@users.noreply.github.com>
2021-11-23 18:44:21 +0900

    finished express tutorial

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
+// static
+app.use(express.static('./methods-public'))
+//parse data
+app.use(express.urlencoded({ extended: false }))
+//parse json
+app.use(express.json())
+
+app.use('/api/people' , people)
+app.use('/login' , auth)
 
 app.listen(3000, () => {
     console.log('listening')
